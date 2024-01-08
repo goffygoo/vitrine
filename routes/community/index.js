d88e76e0288e8ec0863d621379f493668a739880
asifkhan067 <askhan.ak912@gmail.com>
2024-01-09 00:02:39 +0530

    removed redirect from community

--- routes/community/index.js
+++ routes/community/index.js
@@ -1,28 +1,12 @@
 import express from "express";
 import space from "./space.js";
 import user from "./user.js";
-import Monet from "../../service/monet/index.js";
-
 const router = express.Router();
 
 router.get("/", (_req, res) => {
   return res.send({
     health: "OK",
   });
-});
-
-router.post("/paymentRedirect", async (req, res) => {
-  if (req.body.error) {
-    return res.redirect(`http://localhost:3000/confirmpayment?success=false`);
-  }
-  const successObj = await Monet.Order.confirmPayment(
-    req.body.razorpay_payment_id
-  );
-  let success = "Refund";
-  if (successObj.paymentSuccess) success = successObj.paymentSuccess;
-  return res.redirect(
-    `http://localhost:3000/confirmpayment?success=${success}`
-  );
 });
 
 router.use("/space", space);
