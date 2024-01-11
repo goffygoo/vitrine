3b8c8d8fb99ebf595023604f1e14340e5aae9657
asifkhan067 <askhan.ak912@gmail.com>
2024-01-11 22:36:04 +0530

    updated consumer model and update profile api in consumer side

--- routes/consumer/profile.js
+++ routes/consumer/profile.js
@@ -24,12 +24,26 @@ router.post("/update", async (req, res) => {
   const {
     name,
     profilePicture,
+    coverPicture,
+    instagram,
+    x,
+    linkedIn,
+    about,
+    workingHours,
+    offDays,
   } = req.body;
   const { profileId } = res.locals.data;
   try {
     await Consumer.findByIdAndUpdate(profileId, {
       ...(name && { name }),
       ...(profilePicture && { profilePicture }),
+      ...(coverPicture && { coverPicture }),
+      ...(instagram && { instagram }),
+      ...(x && { x }),
+      ...(linkedIn && { linkedIn }),
+      ...(about && { about }),
+      ...(workingHours && { workingHours }),
+      ...(offDays && { offDays }),
     });
     return res.sendStatus(200);
   } catch (_e) {
