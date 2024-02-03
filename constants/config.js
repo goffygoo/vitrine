6d1a6be86f7fdcc27461dd93dc1f3b31359bc7ca
goffygoo <jrvineet52@gmail.com>
2024-02-04 01:30:10 +0530

    config

--- constants/config.js
+++ constants/config.js
@@ -2,17 +2,17 @@ export default {
 	FILE_SERVER: "http://localhost:5002",
 	SEARCH_ENGINE: "http://127.0.0.1:7700",
 	GOOGLE_CLIENT_ID:
-		"611755391410-8cin2sd35dg0o3p04d46a7qn9fpfsjcp.apps.googleusercontent.com",
-	GOOGLE_CLIENT_SECRET: "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",
+		"--GOOGLE_CLIENT_ID--",
+	GOOGLE_CLIENT_SECRET: "--GOOGLE_CLIENT_SECRET--",
 	GOOGLE_TOKEN_URL: "https://oauth2.googleapis.com/token",
 	GOOGLE_USER_INFO_URL:
 		"https://www.googleapis.com/oauth2/v3/userinfo?access_token=",
 	GOOGLE_REDIRECT_URI: "http://localhost:3000/integration/google/callback",
-	JWT_SECRET_KEY: "chillyIsBestFriendMaterial",
-	RAZORPAY_KEY_ID: "rzp_test_QeFBI1VmGEL4NY",
-	RAZORPAY_KEY_SECRET: "hKdjBawiFT4aQxmEXHeN4Rc6",
-	RAZORPAY_WEBHOOK_SECRET: "SMRmeB3z2LyTV3hz0lO2PMygrQr7Njis",
+	JWT_SECRET_KEY: "--JWT_SECRET_KEY--",
+	RAZORPAY_KEY_ID: "--RAZORPAY_KEY_ID--",
+	RAZORPAY_KEY_SECRET: "--RAZORPAY_KEY_SECRET--",
+	RAZORPAY_WEBHOOK_SECRET: "--RAZORPAY_WEBHOOK_SECRET--",
 	GOOGLE_CLIENT_ID_ANDROID: 
-		"719286156722-bs49veig3nkf65n7eel8jalb1j10taue.apps.googleusercontent.com",
-	GOOGLE_CLIENT_SECRET_ANDROID: "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",
+		"--GOOGLE_CLIENT_ID_ANDROID--",
+	GOOGLE_CLIENT_SECRET_ANDROID: "--GOOGLE_CLIENT_SECRET_ANDROID--",
 };
